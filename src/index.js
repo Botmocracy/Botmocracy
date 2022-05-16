56c943c5ec14db5749c90111352c2efc20710962
Dinty1 <dintybmrt@gmail.com>
2022-05-16 15:52:17 +0100

    Semicolon freak alert

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import { Intents, Client } from "discord.js"
+import { Intents, Client } from "discord.js";
 
-require('dotenv').config()
+require('dotenv').config();
 
-const intents = new Intents()
-intents.add(Intents.FLAGS.GUILD_MESSAGES)
-intents.add(Intents.FLAGS.GUILDS)
-intents.add(Intents.FLAGS.GUILD_MEMBERS)
+const intents = new Intents();
+intents.add(Intents.FLAGS.GUILD_MESSAGES);
+intents.add(Intents.FLAGS.GUILDS);
+intents.add(Intents.FLAGS.GUILD_MEMBERS);
 
-const client = new Client({intents: intents})
+const client = new Client({intents: intents});
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
 
 client.on('ready', async() => {
-    console.log(`Bot is ready as ${client.user.username}`)
+    console.log(`Bot is ready as ${client.user.username}`);
 })
