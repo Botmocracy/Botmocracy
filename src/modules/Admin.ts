bb22b8effa58f3fe9fda823f5ae94894686624db
ain <94946723+aindrigo0@users.noreply.github.com>
2022-06-15 08:16:09 -0400

    Town management stuffz (#3)
    * new addtown
    
    * remove pointless log

--- src/modules/Admin.ts
+++ src/modules/Admin.ts
@@ -16,40 +16,7 @@ export default class Admin extends Module {
 
     onModulesLoaded(modules: Collection<string, Module>): void {
         this.commandManager = (modules.get("CommandManager") as CommandManager);
-        this.commandManager.registerCommand({
-            name: "addtown", allowedRoles: ["985426658922201158"], executor(message, args) {
-                if (args.length < 6) {
-                    message.channel.send("Syntax: `addtown <mayor> <depmayor> <x y z> <name>`");
-                    return;
-                }
-
-                const mayor = args.shift(); // TODO: use dc account instead of username
-                const depMayor = args.shift(); // same as above
-
-                const x = args.shift();
-                const y = args.shift();
-                const z = args.shift();
-
-                const name = args.join(' ');
-
-                const coords = `${x} ${y} ${z}`;
-
-                Town.findOne({ name: name }, (err: CallbackError, res: any) => {
-                    if (res) {
-                        Town.deleteOne({ name: name }, (err) => { if (err) message.channel.send(err.toString()) });
-                    }
-                });
-
-                const town = new Town({ name: name, mayor: mayor, depMayor: depMayor, coords: coords });
-
-                town.save(function (err: CallbackError) {
-                    if (err) {
-                        throw err;
-                    }
-                });
-                message.channel.send("Done");
-            }
-        })
+        
     }
 
 }
--- src/schema/Town.ts
+++ src/schema/Town.ts
@@ -3,8 +3,9 @@ import { Schema, model } from 'mongoose';
 const townSchema = new Schema({
     name: String,
     mayor: String,
-    depMayor: {type: String, required: false},
-    coords: String
+    depMayor: {type: String, required: false, default: "None"},
+    coords: String,
+    rank: {type: String, required: false, default: "Unranked"}
 })
 
 export default model("Town", townSchema);
