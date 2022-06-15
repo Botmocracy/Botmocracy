69f9fdd2549ce7ffa688cc43bdbcba9b23c21403
ain <94946723+aindrigo0@users.noreply.github.com>
2022-06-15 08:16:09 -0400

    Town management stuffz (#3)
    * new addtown
    
    * remove pointless log

--- src/modules/Info.ts
+++ src/modules/Info.ts
@@ -2,6 +2,8 @@ import { Collection, MessageEmbed } from "discord.js";
 import Town from "../schema/Town";
 import Module from "./abstract/Module";
 import CommandManager from "./CommandManager";
+import { request } from 'undici';
+import { CallbackError } from "mongoose";
 
 
 export default class Info extends Module {
@@ -11,8 +13,30 @@ export default class Info extends Module {
     onEnable(): void {
         this.logger.info("Enabled");
     }
-  
-    onReady(modules: Collection<string, Module>): void {
+    
+            
+    async getTownByName(name: string) {
+        const result = await request("https://script.google.com/macros/s/AKfycbwde4vwt0l4_-qOFK_gL2KbVAdy7iag3BID8NWu2DQ1566kJlqyAS1Y/exec?spreadsheetId=1JSmJtYkYrEx6Am5drhSet17qwJzOKDI7tE7FxPx4YNI&sheetName=New%20World", {maxRedirections: 1});
+        let fullBody = '';
+
+        for await (const data of result.body) {
+            fullBody += data.toString(); // ok but why doesn't it just send it as a string...
+        }
+
+        const towns: [{[key: string]: any}] = JSON.parse(fullBody);
+        
+        for(const townData of towns) {
+            if(townData['Town Name'] == name) {
+                return townData;
+            }
+        }
+
+        return undefined;
+    }
+
+    onModulesLoaded(modules: Collection<string, Module>): void {
+        const self = this;
+
         this.commandManager = (modules.get("CommandManager") as CommandManager);  
         this.commandManager.registerCommand({name: "gettown", executor(message, args) {
             if(args.length < 1) {
@@ -38,6 +62,41 @@ export default class Info extends Module {
                 
                 message.channel.send({embeds: [embed]});
             })
+        }});
+
+        this.commandManager.registerCommand({name: "addtown", executor(message, args) {
+            if(args.length < 1) {
+                message.channel.send("Syntax: `addtown <name`");
+            }
+            self.getTownByName(args[0]).then(result => {
+                if(!result) {
+                    message.channel.send("Invalid town");
+                }
+                result = (result as {[key: string]: any})
+                const townName = result["Town Name"];
+
+                const town = new Town({
+                    name: result["Town Name"],
+                    mayor: result['Mayor'], 
+                    depMayor: result['Deputy Mayor'], 
+                    coords: `${result['X']} ${result['Y']} ${result['Z']}`, 
+                    rank: result['Town Rank']
+                })
+
+                Town.findOne({name: townName}, (err: CallbackError, res: any) => {
+                    if(!err || res) {
+                        Town.deleteOne({name: townName}, (err: CallbackError, res: any) => {
+                            if(err) throw err;
+                        })
+                    }
+                })
+
+                town.save((err: CallbackError) => {if (err) throw err;});
+
+                message.channel.send("Done.");
+            })
+
         }})
     }
+
 }
