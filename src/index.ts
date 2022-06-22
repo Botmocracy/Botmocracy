62228596eb8ca077f50dacec8369befe76c90ca8
Dinty1 <dintybmrt@gmail.com>
2022-06-22 22:48:47 +0100

    >:(

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,11 @@ import { readdirSync } from "fs";
 import * as mongoose from 'mongoose';
 import { Dispatcher } from "undici";
 import Module from "./modules/abstract/Module";
+import Logger from "./util/Logger";
 
 dotenv.config();
 
-mongoose.connect((process.env.MONGO_STRING as string))
+mongoose.connect((process.env.MONGO_STRING as string));
 
 const intents = new Intents();
 intents.add(Intents.FLAGS.GUILD_MESSAGES);
@@ -17,6 +18,8 @@ intents.add(Intents.FLAGS.GUILDS);
 intents.add(Intents.FLAGS.GUILD_MEMBERS);
 
 const client = new Client({ intents: intents });
+
+const logger = new Logger("Index");
 
 export const modules = new Collection<string, Module>();
 
@@ -26,11 +29,11 @@ client.on('ready', async () => {
     const moduleFiles = readdirSync("src/modules");
 
     const rest = new REST({ version: '10' }).setToken(process.env.TOKEN!);
-    let slashCommands: RESTPostAPIApplicationCommandsJSONBody[] = []
+    let slashCommands: RESTPostAPIApplicationCommandsJSONBody[] = [];
 
     for (const f of moduleFiles) {
         if (!f.endsWith(".ts")) continue; // Ignore non-ts files
-        const M = require(`./modules/${f}`)
+        const M = require(`./modules/${f}`);
         const module = new M.default();
         if (!(module instanceof Module)) throw new Error(`Module ${f} does not extend "Module"`);
         modules.set(module.name, module);
@@ -45,7 +48,7 @@ client.on('ready', async () => {
 	    Routes.applicationCommands(client!.user!.id),
 	    { body: slashCommands },
     );
-    console.log(`${slashCommands.length} application commands reloaded`)
+    logger.info(`${slashCommands.length} application commands reloaded`)
 });
 
 client.login(process.env.TOKEN);
