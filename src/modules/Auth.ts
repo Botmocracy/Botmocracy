44213d078b2e7285c57ecbe3677eb5214909179b
Dinty1 <dintybmrt@gmail.com>
2022-06-30 17:56:23 +0100

    Formatting

--- src/modules/Auth.ts
+++ src/modules/Auth.ts
@@ -6,11 +6,11 @@ import Module from "./abstract/Module";
 
 export default class Auth extends Module {
     name = "Auth";
-    
+
     onEnable(): void {
         this.logger.info("Enabled");
     }
-    
+
     async getMembers() {
         const { data } = await axios.get("https://script.google.com/macros/s/AKfycbwde4vwt0l4_-qOFK_gL2KbVAdy7iag3BID8NWu2DQ1566kJlqyAS1Y/exec?spreadsheetId=1Hhj_Cghfhfs8Xh5v5gt65kGc4mDW0sC5GWULKidOBW8&sheetName=Members");
         let res: string[] = [];
@@ -23,7 +23,7 @@ export default class Auth extends Module {
 
     async getName(uuid: string) {
         const { data, status } = await axios.get(`https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`);
-        if(status == 404) {
+        if (status == 404) {
             return null;
         }
 
@@ -35,24 +35,24 @@ export default class Auth extends Module {
             cmdBuilder: new SlashCommandBuilder()
                 .setName("verify")
                 .setDescription("Verifies you"),
-            
+
             executor: async (i: CommandInteraction) => {
-                if(!i.guild) {
-                    i.reply({content: "You can't use this in a DM", ephemeral: true});
+                if (!i.guild) {
+                    i.reply({ content: "You can't use this in a DM", ephemeral: true });
                     return;
                 }
 
                 const member = i.guild.members.cache.get(i.user.id);
 
                 if (member?.roles.cache.has("987775509368811530")) {
-                    i.reply({content: "You're already verified.", ephemeral: true});
+                    i.reply({ content: "You're already verified.", ephemeral: true });
                     return;
                 }
 
                 const req = await axios.get(`https://minecraftauth.me/api/lookup?discord=${i.user.id}`);
 
                 if (req.status == 404) {
-                    i.reply({content: "You need to verify with https://minecraftauth.me first."});
+                    i.reply({ content: "You need to verify with https://minecraftauth.me first." });
                     return;
                 }
 
@@ -60,9 +60,9 @@ export default class Auth extends Module {
                 const uuid = data.minecraft.identifier;
                 const members = await this.getMembers();
                 const name = await this.getName(uuid);
-                
-                if(name == null) {
-                    i.reply({content: "I couldn't get your username for some reason. Try again.", ephemeral: true});
+
+                if (name == null) {
+                    i.reply({ content: "I couldn't get your username for some reason. Try again.", ephemeral: true });
                     return;
                 }
 
@@ -70,9 +70,9 @@ export default class Auth extends Module {
                     const role = i.guild.roles.cache.get("987775509368811530");
                     member?.roles.add((role as Role));
 
-                    i.reply({content: "Verified!", ephemeral: true});
+                    i.reply({ content: "Verified!", ephemeral: true });
                 }
-                
+
             }
         }
     }
