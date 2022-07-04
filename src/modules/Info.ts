8735078ffc4a2b60daa30608599f6b60871f0f97
7d <i.second.7d@gmail.com>
2022-07-04 19:13:26 +0800

    combine town commands

--- src/modules/Info.ts
+++ src/modules/Info.ts
@@ -33,78 +33,84 @@ export default class Info extends Module {
         return undefined;
     }
     slashCommands = {
-        gettown: {
+        town: {
             cmdBuilder: new SlashCommandBuilder()
-                .setName("gettown")
-                .setDescription("Get info about a town")
-                .addStringOption(option =>
-                    option.setName("name")
-                        .setDescription("Name of the town")
-                        .setRequired(true)
-                ),
-            executor: async (i: CommandInteraction) => {
-                await i.deferReply();
-                Town.findOne({ name: i.options.getString("name") }, async (err: any, res: any) => {
-                    if (!res || err) {
-                        await i.editReply(`Invalid town \`${i.options.getString("name")}\``);
-                        return;
-                    }
-                    const name = res['name'];
-                    const mayor = res['mayor'];
-                    const depMayor = res['depMayor'];
-                    const coords = res['coords'];
-
-                    const embed = new MessageEmbed()
-                        .setTitle(name)
-                        .addField("Mayor", mayor)
-                        .addField("Deputy Mayor", depMayor)
-                        .addField("Coords", coords)
-                        .setColor("BLURPLE");
-
-                    await i.editReply({ embeds: [embed] });
-                })
-            }
-        },
-        addtown: {
-            cmdBuilder: new SlashCommandBuilder()
-                .setName("addtown")
-                .setDescription("Add a town")
-                .addStringOption(option =>
-                    option.setName("name")
-                        .setDescription("Name of the town")
-                        .setRequired(true)
-                ),
-            executor: async (i: CommandInteraction) => {
-                await i.deferReply();
-                let result = await this.getTownByName(i.options.getString("name") ?? "")
-                if (!result) {
-                    await i.editReply(`Invalid town \`${i.options.getString("name")}\``);
-                    return;
-                }
-                result = (result as { [key: string]: any })
-                const townName = result["Town Name"];
-
-                const town = new Town({
-                    name: result["Town Name"],
-                    mayor: result['Mayor'],
-                    depMayor: result['Deputy Mayor'],
-                    coords: `${result['X']} ${result['Y']} ${result['Z']}`,
-                    rank: result['Town Rank']
-                })
-
-                Town.findOne({ name: townName }, (err: CallbackError, res: any) => {
-                    if (!err || res) {
-                        Town.deleteOne({ name: townName }, (err: CallbackError, res: any) => {
-                            if (err) throw err;
+                .setName("town")
+                .setDescription("Add or get info about a town")
+                .addSubcommand(s => s
+                    .setName("get")
+                    .setDescription("Get info about a town")
+                    .addStringOption(option =>
+                        option.setName("name")
+                            .setDescription("Name of the town")
+                            .setRequired(true)
+                ))
+                .addSubcommand(s => s
+                    .setName("add")
+                    .setDescription("Add a town")
+                    .addStringOption(option =>
+                        option.setName("name")
+                            .setDescription("Name of the town")
+                            .setRequired(true)
+                )),
+            subcommands: {
+                get: {
+                    executor: async (i: CommandInteraction) => {
+                        await i.deferReply();
+                        Town.findOne({ name: i.options.getString("name") }, async (err: any, res: any) => {
+                            if (!res || err) {
+                                await i.editReply(`Invalid town \`${i.options.getString("name")}\``);
+                                return;
+                            }
+                            const name = res['name'];
+                            const mayor = res['mayor'];
+                            const depMayor = res['depMayor'];
+                            const coords = res['coords'];
+        
+                            const embed = new MessageEmbed()
+                                .setTitle(name)
+                                .addField("Mayor", mayor)
+                                .addField("Deputy Mayor", depMayor)
+                                .addField("Coords", coords)
+                                .setColor("BLURPLE");
+        
+                            await i.editReply({ embeds: [embed] });
                         })
                     }
-                })
-
-                town.save((err: CallbackError) => { if (err) throw err; });
-
-                await i.editReply(`Added town \`${i.options.getString("name")}\``);
+                },
+                add: {
+                    executor: async (i: CommandInteraction) => {
+                        await i.deferReply();
+                        let result = await this.getTownByName(i.options.getString("name") ?? "")
+                        if (!result) {
+                            await i.editReply(`Invalid town \`${i.options.getString("name")}\``);
+                            return;
+                        }
+                        result = (result as { [key: string]: any })
+                        const townName = result["Town Name"];
+        
+                        const town = new Town({
+                            name: result["Town Name"],
+                            mayor: result['Mayor'],
+                            depMayor: result['Deputy Mayor'],
+                            coords: `${result['X']} ${result['Y']} ${result['Z']}`,
+                            rank: result['Town Rank']
+                        })
+        
+                        Town.findOne({ name: townName }, (err: CallbackError, res: any) => {
+                            if (!err || res) {
+                                Town.deleteOne({ name: townName }, (err: CallbackError, res: any) => {
+                                    if (err) throw err;
+                                })
+                            }
+                        })
+        
+                        town.save((err: CallbackError) => { if (err) throw err; });
+        
+                        await i.editReply(`Added town \`${i.options.getString("name")}\``);
+                    }
+                }
             }
         }
-
     }
 }
