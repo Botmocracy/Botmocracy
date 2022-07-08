ed8b392d01ac7019089ac8ce9a4d37a07a221a96
Dinty1 <dintybmrt@gmail.com>
2022-07-08 22:59:31 +0100

    Do some shit

--- src/modules/ElectionCounter.ts
+++ src/modules/ElectionCounter.ts
@@ -31,7 +31,7 @@ export default class ElectionCounter extends Module {
 
         const votesRaw = await ElectionVote.find().exec();
         for (const vote of votesRaw) {
-            let ballot = (vote.preferences! as Array<string>).filter(p => this.candidates.includes(p)) /*I've already said this in the schema why do you need it a second time...?*/;
+            let ballot = (vote.preferences! as Array<string>/*I've already said this in the schema why do you need it a second time...?*/).filter(p => this.candidates.includes(p));
             if (ballot.length == 0) return;
 
             if (!this.votes[ballot[0]]) this.votes[ballot[0]] = [];
