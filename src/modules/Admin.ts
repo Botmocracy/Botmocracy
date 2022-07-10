0e1e71256f099c010f3e946c5d0545e01b7ae6b2
Dinty1 <dintybmrt@gmail.com>
2022-07-10 06:48:46 +0100

    Use config && formatting

--- src/modules/Admin.ts
+++ src/modules/Admin.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
+import { config } from "..";
 import Module from "./abstract/Module";
 
 export default class Admin extends Module {
@@ -13,22 +14,22 @@ export default class Admin extends Module {
         superuser: {
             cmdBuilder: new SlashCommandBuilder().setName("superuser").setDescription("Adds le admin role. Can only be used by binty, ain and fishe"),
             async executor(i: CommandInteraction) {
-                if(!i.inGuild()) return;
+                if (!i.inGuild()) return;
                 const allowedPeople = ["644052617500164097", "468534859611111436", "716779626759716914"];
-                if(!allowedPeople.includes(i.user.id)) return i.reply({content: "You cannot use this.", ephemeral: true});
+                if (!allowedPeople.includes(i.user.id)) return i.reply({ content: "You cannot use this.", ephemeral: true });
 
-                const role = i.guild!.roles.cache.get("985426658922201158");
+                const role = i.guild!.roles.cache.get(config.admin_role);
                 const member = i.guild!.members.cache.get(i.user.id);
 
-                if(member?.roles.cache.has("985426658922201158")) {
+                if (member?.roles.cache.has(config.admin_role)) {
                     member.roles.remove(role!);
-                    return i.reply({content: "Done.", ephemeral: true});
+                    return i.reply({ content: "Done.", ephemeral: true });
                 }
 
                 member?.roles.add(role!);
-                return i.reply({content: "Done.", ephemeral: true});
+                return i.reply({ content: "Done.", ephemeral: true });
             }
         }
-        
+
     }
 }
