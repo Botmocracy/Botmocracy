ee1dec397084787d45bd687a3eb3f4292de3db1f
Dinty1 <dintybmrt@gmail.com>
2023-12-31 10:37:21 +0000

    Log all interactions

--- src/index.ts
+++ src/index.ts
@@ -54,4 +54,11 @@ client.on('ready', async (client) => {
     logger.info(`${slashCommands.length} application commands reloaded`);
 });
 
+client.on("interactionCreate", i => {
+    if (!i.guild) return;
+    if (i.guild.id != config.guild) return;
+
+    logger.info(`Interaction received with type ${i.type} and id ${(i as any).customId} from ${i.user.tag}`);
+});
+
 client.login(process.env.TOKEN);
